8695b69daa224692b0050f52bd5f6092497fadc5
Simon Mei <smei8800@gmail.com>
2019-05-06 16:33:38 -0400

    merged up

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-<<<<<<< HEAD
-const link = {
-  width: "100px",
-  padding: "12px",
-  margin: "0 6px 6px",
-  background: "blue",
-  textDecoration: "none",
-  color: "white"
-};
-
-=======
->>>>>>> 0a79967afd21a400c98be75ea42dcc00db1e7ff9
 const NavBar = () => {
   return (
     <div className="navbar">
