d75891d586454cdef954a3bba47ec1c823805ca8
Simon Mei <smei8800@gmail.com>
2019-05-06 11:41:25 -0400

    routing situation

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NavBar from "./components/NavBar";
 import "./App.css";
 import FoodContainer from './containers/FoodContainer'
 import About from './components/About'
+import Contact from './components/Contact'
 
 export default class App extends React.Component {
 
@@ -25,11 +26,33 @@ export default class App extends React.Component {
 
   render() {
     return (
-        <div>
+      <Router>
+        <div className="App">
           <NavBar />
-          <CategoryList handleCategoryClick={this.handleCategoryClick}/>
-          <FoodContainer categoryChoice={this.state.categoryChoice}/>
+          {/* Route to Menu page */}
+          <Route path='/' exact render={
+            () => {
+              return(
+                <div className="menu">
+                  <CategoryList handleCategoryClick={this.handleCategoryClick}/>
+                  <FoodContainer categoryChoice={this.state.categoryChoice}/>
+                </div>
+              )
+            }
+          }
+          />
+        {/* Route to About page */}
+        <Route exact path='/about' render={About}/>
+        {/* Route to Contact page */}
+        <Route exact path='/contact' render={Contact}/>
         </div>
+      </Router>
     );
   }
 }
+
+
+
+
+
+// Notes
