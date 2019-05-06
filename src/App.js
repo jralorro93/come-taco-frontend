091cf67bf5ffd18ad08a611485f97dd46a48a07d
github username <github email address>
2019-05-05 20:09:04 -0400

    tried testing out the fetch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import Categorylist from './containers/CategoryList'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/items')
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
+  render() {
+    return null
+  }
 }
-
-export default App;
---
+++ src/containers/CategoryList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class CategoryList extends React.Component {
+  render() {
+    return null
+  }
+}
