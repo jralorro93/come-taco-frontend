0a79967afd21a400c98be75ea42dcc00db1e7ff9
github username <github email address>
2019-05-06 16:09:00 -0400

    added a Nav Bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const link = {
-  width: "100px",
-  padding: "12px",
-  margin: "0 6px 6px",
-  background: "blue",
-  textDecoration: "none",
-  color: "white"
-}
-
 const NavBar = () => {
   return (
     <div className="navbar">
-      <p>About</p>
-      <p>Contact Us</p>
-      <p>Login</p>
+    {/*Drop down options*/}
+      <ul id="dropdown1" class="dropdown-content">
+        <li><a href="#!">one</a></li>
+        <li><a href="#!">two</a></li>
+        <li class="divider"></li>
+        <li><a href="#!">three</a></li>
+      </ul>
+        <nav>
+        {/*Nav Bar*/}
+          <div class="nav-wrapper">
+            <NavLink to='/' className="brand-logo center">Come Taco</NavLink>
+            <ul class="right hide-on-med-and-down">
+              <li><NavLink to='/about'>About</NavLink></li>
+              <li><NavLink to='/contact'>Contact Us</NavLink></li>
+              <li><NavLink to='/login'>Login</NavLink></li>
+            {/*<!-- Dropdown Trigger -->*/}
+              <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li>
+            </ul>
+          </div>
+        </nav>
     </div>
   )
 }
