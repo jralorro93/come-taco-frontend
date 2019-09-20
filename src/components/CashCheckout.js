bcd867deb4978d4ab7b754d9567fcc3de7bd1d66
github username <github email address>
2019-09-20 15:24:23 -0400

    working on checkout page

---
+++ src/components/CashCheckout.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const CashCheckout = () => {
+    return (
+        <div>Hi from CashCheckout</div>
+    )
+}
+export default CashCheckout
---
+++ src/components/CreditInfo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class CreditInfo extends Component {
+    render() {
+        return (
+            <div>
+                Hi from CreditInfo
+            </div>
+        )
+    }
+}
+export default CreditInfo
