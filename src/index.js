f34e01f8734a8dd546b6e81ebdfdd0a51ffeb24d
github username <github email address>
2019-11-03 17:31:28 -0500

    working on themes

--- src/index.js
+++ src/index.js
@@ -3,9 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+//Routers
 import { BrowserRouter as Router, Route, withRouter } from "react-router-dom";
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+//Themes
+import { ThemeProvider } from '@material-ui/core/styles'
+import theme from './theme/muiTheme'
+
+
+ReactDOM.render(
+    <Router>
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
+    </Router>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
