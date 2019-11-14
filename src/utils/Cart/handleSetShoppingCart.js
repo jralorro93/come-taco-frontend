63b83d9c014f9d69adefc07a2bbac705d4076401
github username <github email address>
2019-11-14 14:24:56 -0500

    refactoring receipt card

--- src/utils/Cart/handleSetShoppingCart.js
+++ src/utils/Cart/handleSetShoppingCart.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
-const handleSetShoppingCart = ( ) => {
-
+const handleSetShoppingCart = () => {
+    // const [ shoppingCart, setShoppingCart ] = useState([])
+    // useEffect( () => {
+    //     const fetchFood = async () => {
+    //         const result = await axios('http://localhost:3000/api/v1/get_items')
+    //         setShoppingCart(result.data.user.shoppingCart)
+    //     }
+    //     fetchFood()
+    // }, [shoppingCart])
+    // return (
+    //     <div>Hi</div>
+    // )
 }
 export default handleSetShoppingCart
