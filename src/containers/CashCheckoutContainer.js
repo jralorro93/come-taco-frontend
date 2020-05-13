579704bf5e9491b9a825e2722159621a0b97b41f
github username <github email address>
2020-05-12 23:36:06 -0400

    fixing cash co

--- src/containers/CashCheckoutContainer.js
+++ src/containers/CashCheckoutContainer.js
@@ -1,8 +1,23 @@
 import React from 'react';
 
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+    container: {
+        backgroundColor: theme.palette.common.white,
+        height: '500px',
+        width: '500px',
+
+    },
+    image: {
+
+    }
+}))
+
 const CashCheckoutContainer = () => {
+    const classes = useStyles()
     return (
-        <div>Hi from CashCheckout</div>
+        <div className={classes.container}>Hi from CashCheckout</div>
     )
 }
 export default CashCheckoutContainer
