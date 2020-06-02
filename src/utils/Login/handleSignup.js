c3b4ab96fbaf51b73bed9b592b55bc5b1dbb8892
github username <github email address>
2020-06-02 13:17:25 -0400

    refactored signup

---
+++ src/utils/Login/handleSignup.js
@@ -0,0 +1,22 @@
+const handleSignup = (email, password, firstName, lastName, setCurrentUser, localStorage, history) => {
+    fetch('http://localhost:3000/api/v1/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          first_name: firstName,
+          last_name: lastName
+        })
+    })
+    .then(res => res.json())
+    .then(user => {
+        localStorage.setItem('token', user.jwt)
+        setCurrentUser(user.user, () => history.push('/'))
+    })
+}
+
+export default handleSignup
