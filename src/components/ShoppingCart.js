ee7cc3b6134dfbffc883f7351923de816c842128
github username <github email address>
2020-06-12 01:26:59 -0400

    refactoring cart

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -4,11 +4,11 @@ import ReceiptContainer from '../containers/ReceiptContainer'
 import CartTotal from './CartTotal'
 
 const ShoppingCart = (props) => {
-    console.log('this is props', props)
+
     return (
       <div className='ShoppingCartPage'>
         <ReceiptContainer shoppingCart={props.shoppingCart}  handleDelete={props.handleDelete}/>
-        <CartTotal  shoppingCart={props.shoppingCart} history={props.history}/>
+        {/* <CartTotal  shoppingCart={props.shoppingCart} history={props.history}/> */}
       </div>
     )
 }
