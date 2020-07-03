dd28d5583bf6bf2667e9c9a08b91632f8d4eeab1
github username <github email address>
2020-07-03 16:04:37 -0400

    refactoring stripe

--- src/components/CreditForm2.js
+++ src/components/CreditForm2.js
@@ -6,15 +6,21 @@ import { useStripe, useElements, CardElement } from '@stripe/react-stripe-js';
 
 import CardSection from './CardSection'
 import stripeTokenHandler from '../utils/Checkout/stripeTokenHandler'
-import handleGrandTotal from '../utils/Checkout/handleGrandTotal'
-import handlePayment from '../utils/Checkout/handlePayment'
+import {handleGrandTotal, handlePayment} from '../utils/Checkout/handleGrandTotal'
 import { UserContext } from '../App2'
 import { regHeaders } from '../utils/headers'
 
 
+const info = {
+    user_id: 1,
+    price: 2000,
+    items: ['']
+}
+
 const CreditForm = () => {
     // CREATE PAYMENT THEN CHARGE CHECK FIRST TIME
     // MAYBE???
+
     const [values, setValues] = useState({
         succeeded: '',
         error: '',
@@ -25,27 +31,27 @@ const CreditForm = () => {
     const stripe = useStripe()
     const elements = useElements()
 
-    const stripeTokenHandler = (token, localStorage) => {
-        // const paymentData = {
-        //     token: token.id
-        // }
-        console.log('token', token)
-        // console.log('paymentData', paymentData)
-        const body = {
-            name: 'Juan Alorro',
-            amount: 19.20
-        }
-        const response = axios({
-            method: 'post',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            url: 'http://localhost:3000/api/v1/charges',
-            body: JSON.stringify(body)
-        })
+    // const stripeTokenHandler = (token, localStorage) => {
+    //     // const paymentData = {
+    //     //     token: token.id
+    //     // }
+    //     console.log('token', token)
+    //     // console.log('paymentData', paymentData)
+    //     const body = {
+    //         name: 'Juan Alorro',
+    //         amount: 19.20
+    //     }
+    //     const response = axios({
+    //         method: 'post',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //             'Accept': 'application/json'
+    //         },
+    //         url: 'http://localhost:3000/api/v1/charges',
+    //         body: JSON.stringify(body)
+    //     })
         
-    }
+    // }
 
     // const handleSubmit = async (e) => {
     //     e.preventDefault()
@@ -81,9 +87,25 @@ const CreditForm = () => {
     //     console.log(result)
         
     // }
+
+
+    // THE IDEA IS TO CREATE A PAYMENT INTENT UPON MOUNT
+    // THEN TO SEND A PAYMENT CONFIRMATION ON CLICK
+    useEffect(()=> {
+        const fetchData = async () => {
+            const result = await fetch('http://localhost:3000/api/v1/secrets', {
+                method: 'POST',
+                regHeaders,
+                body: JSON.stringify(info)
+            })
+            console.log('this is the result', result)
+        }
+
+        fetchData()
+    }, [])
     
     return (
-        <form onSubmit={(e) => handleSubmit(e)}>
+        <form >
             <CardSection />
             <button disabled={!stripe}>Confirm Order</button>
         </form>
@@ -91,3 +113,4 @@ const CreditForm = () => {
 }
 
 export default CreditForm
+// onSubmit={(e) => handleSubmit(e)}
