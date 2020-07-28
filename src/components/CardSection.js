d1ba0ad412f99314aee7ea7798d7d9dbc8fe13fe
github username <github email address>
2020-07-27 21:39:56 -0400

    Styling Checkout

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -1,29 +1,11 @@
 import React from 'react'
 import {CardElement} from '@stripe/react-stripe-js'
 
-const CARD_ELEMENT_OPTIONS = {
-  style: {
-    base: {
-      color: "#32325d",
-      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-      fontSmoothing: "antialiased",
-      fontSize: "16px",
-      "::placeholder": {
-        color: "#aab7c4",
-      },
-    },
-    invalid: {
-      color: "#fa755a",
-      iconColor: "#fa755a",
-    },
-  },
-};
-
-function CardSection() {
+ const CardSection = () => {
   return (
     <label>
       Card details
-      <CardElement options={CARD_ELEMENT_OPTIONS} />
+      <CardElement/>
     </label>
   );
 };
