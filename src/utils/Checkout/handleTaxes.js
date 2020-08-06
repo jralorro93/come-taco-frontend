115518e1718a13e7a95e6424d3a84d19d53a492c
github username <github email address>
2020-08-06 15:51:57 -0400

    refactoring checkout

--- src/utils/Checkout/handleTaxes.js
+++ src/utils/Checkout/handleTaxes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const handleTaxes = (cart) => {
-    let total = 0;
-    for (let i = 0; i < cart.length; i++) {
-        total += cart[i].price
+    if (cart) {
+        let total = 0;
+        for (let i = 0; i < cart.length; i++) {
+            total += cart[i].price
+        }
+        let tax = total * .0875
+        return tax.toFixed(2)
     }
-    let tax = total * .0875
-    return tax.toFixed(2)
 }
 export default handleTaxes
